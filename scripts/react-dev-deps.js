56408ad1692ea4b7d8ec1e28fc3b4aa762295b16
Law Horne <foxtrottwist@ymail.com>
2018-11-04 22:22:15 -0500

    eslintConfig, husky and lint-stage now being added to package.json

--- scripts/react-dev-deps.js
+++ scripts/react-dev-deps.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const { spawnSync, } = require('child_process')
+const path = require('path')
+const os = require('os')
+const { spawnSync, execSync } = require('child_process')
+const cwd = process.cwd()
 
 // Edit dev dependencies here
 const devPkgs = [
@@ -12,84 +15,106 @@ const devPkgs = [
   'eslint-plugin-react-hooks',
   '-D',
 ]
-const [, , option,] = process.argv
+
+// Determine whether to use yarn or npm
+const [, , option] = process.argv
 const cmd = option === '-N' ? 'npm' : 'yarn'
-const args = cmd === 'yarn' ? ['add', ...devPkgs,] : ['install', ...devPkgs,]
+const args = cmd === 'yarn' ? ['add', ...devPkgs] : ['install', ...devPkgs]
 
 const isCreateReactApp = fs.existsSync('node_modules/react-scripts')
 const isGastbyjs = fs.existsSync('node_modules/gatsby')
 
+// package.json configurations
+const packagejson = require(path.join(cwd, 'package.json'))
+const husky = {
+  hooks: {
+    'pre-commit': 'lint-staged',
+  },
+}
+const lint = {
+  'lint-staged': {
+    'src/**/*.{js,jsx,json,css}': ['prettier --write', 'git add'],
+  },
+}
+
+const prettierrc = `{
+  "printWidth": 80,
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": false,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "arrowParens": "avoid",
+  "requirePragma": false,
+  "insertPragma": false,
+  "proseWrap": "preserve"
+}
+`
+
 if (isCreateReactApp) {
-  spawnSync(cmd, args, { stdio: 'inherit', })
+  spawnSync(cmd, args, { stdio: 'inherit' })
 
-  writePrettierConfig()
+  const eslintConfig = {
+    extends: ['react-app', 'prettier/react', 'prettier'],
+    plugins: ['react-hooks'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'comma-dangle': ['error', 'always'],
+    },
+  }
+
+  fs.writeFileSync(
+    path.join(cwd, 'package.json'),
+    JSON.stringify({ ...packagejson, eslintConfig, husky, ...lint }, null, 2) +
+      os.EOL,
+  )
+
+  fs.writeFileSync(path.join(cwd, '.prettierrc'), prettierrc)
 } else if (isGastbyjs) {
-  const gatsbyArgs = [...args, 'babel-eslint', 'eslint', 'eslint-plugin-react',]
+  const gatsbyArgs = [...args, 'babel-eslint', 'eslint', 'eslint-plugin-react']
 
-  spawnSync(cmd, gatsbyArgs, { stdio: 'inherit', })
+  execSync('git init', { stdio: 'ignore' })
+  execSync('git add -A', { stdio: 'ignore' })
+  execSync('git commit -m "Initial commit"', { stdio: 'ignore' })
+  spawnSync(cmd, gatsbyArgs, { stdio: 'inherit' })
 
-  writeEslinConfig()
+  const eslintConfig = {
+    parser: 'babel-eslint',
+    rules: {
+      strict: 0,
+      'no-ternary': 'off',
+      'comma-dangle': ['error', 'always'],
+      'react/prop-types': 0,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'prettier/react',
+      'prettier',
+    ],
+    parserOptions: {
+      ecmaVersion: 8,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    env: {
+      es6: true,
+      browser: true,
+      node: true,
+    },
+  }
 
-  writePrettierConfig()
+  fs.writeFileSync(
+    path.join(cwd, 'package.json'),
+    JSON.stringify({ ...packagejson, eslintConfig, husky, ...lint }, null, 2) +
+      os.EOL,
+  )
+
+  fs.writeFileSync(path.join(cwd, '.prettierrc'), prettierrc)
 } else {
   console.log('You appear to not be in a Create-React-App or GatsbyJS project.')
-}
-
-function writePrettierConfig() {
-  const prettierConfig = `{
-    "printWidth": 80,
-    "tabWidth": 2,
-    "useTabs": false,
-    "semi": false,
-    "singleQuote": true,
-    "trailingComma": "all",
-    "bracketSpacing": true,
-    "jsxBracketSameLine": false,
-    "arrowParens": "avoid",
-    "requirePragma": false,
-    "insertPragma": false,
-    "proseWrap": "preserve"
-  }
-  `
-  try {
-    fs.writeFileSync('.prettierrc', prettierConfig)
-  } catch (error) {
-    // ignoring
-  }
-}
-
-function writeEslinConfig() {
-  const eslintConfig = `{
-    "parser": "babel-eslint",
-    "rules": {
-      "strict": 0,
-      "no-ternary": "off",
-      "comma-dangle": ["error", "always"],
-      "react/prop-types": 0
-    },
-    "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "prettier/react",
-      "prettier"
-    ],
-    "parserOptions": {
-      "ecmaVersion": 8,
-      "sourceType": "module",
-      "ecmaFeatures": {
-        "jsx": true
-      }
-    },
-    "env": {
-      "es6": true,
-      "browser": true,
-      "node": true
-    }
-  }`
-
-  try {
-    fs.writeFileSync('.eslintrc.json', eslintConfig)
-  } catch (error) {
-    // ignoring
-  }
 }
